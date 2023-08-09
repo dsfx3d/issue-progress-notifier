b985857b7f354bad1128684855d06c3d5cfab5f2
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-09 21:15:16 +0530

    🎉 tada!!

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,21 @@
-/* eslint-env node */
 module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
   ],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jasmine: true,
+  },
+  globals: {
+    NodeJS: true,
+  },
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: "./tsconfig.json",
-    ecmaVersion: 12,
+    ecmaVersion: "latest",
     sourceType: "module",
   },
   plugins: ["@typescript-eslint"],
