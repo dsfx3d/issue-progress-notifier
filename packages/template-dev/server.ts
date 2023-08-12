2f3123f65ce9dbd5b96768ecc79d162e42b8d767
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-12 10:50:51 +0530

    feat(graphql-request): :tada: add curry for reusability and easy compositions (#23)

--- packages/template-dev/server.ts
+++ packages/template-dev/server.ts
@@ -1,13 +1,20 @@
 import express from "express";
+import {pipe} from "fp-ts/function";
 import {toIssuesRouter} from "./issues/toIssuesRouter";
 import {toGraphQLClient} from "./toGraphQLClient";
 import {env} from "./env";
+import {toGetIssueQuery} from "./issues/toGetIssueQuery";
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(
   "/issues",
-  toIssuesRouter(express.Router(), toGraphQLClient(env.GH_ACCESS_TOKEN)),
+  pipe(
+    env.GH_ACCESS_TOKEN,
+    toGraphQLClient,
+    toGetIssueQuery,
+    toIssuesRouter(express.Router()),
+  ),
 );
 app.listen(3000, () => {
   console.log("Listening on port http://localhost:3000");
