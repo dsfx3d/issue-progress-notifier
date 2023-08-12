dc1bc06978612a531e1c8fd8bfcb0f9b1a4269cd
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-13 03:09:50 +0530

    feat(compiler): :sparkles: make compiler lazy (#6)

--- packages/template-compiler/src/toTemplateCompiler.ts
+++ packages/template-compiler/src/toTemplateCompiler.ts
@@ -1,11 +1,14 @@
 import {type CompileOptions} from "./CompileOptions";
 import {type TCompiler} from "./TCompiler";
+import {TCompilerTask} from "./TCompilerTask";
 import {flow} from "fp-ts/lib/function";
+import {of} from "fp-ts/lib/Task";
+import {toCompileOptions} from "./toCompileOptions";
 
 export function toTemplateCompiler<TProps>(
   toHtml: TCompiler<CompileOptions>,
   toBody: TCompiler<TProps>,
   css: string,
-): TCompiler<TProps> {
-  return flow(toBody, body => toHtml({body, css}));
+): TCompilerTask<TProps> {
+  return flow(toBody, toCompileOptions({css}), toHtml, of);
 }
