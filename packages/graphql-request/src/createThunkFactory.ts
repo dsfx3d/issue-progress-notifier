bb21a8246f454ef99bea381a0c70213832be9f1b
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-12 10:49:49 +0530

    feat(graphql-request): :tada: add curry for reusability and easy compositions (#23)

---
+++ packages/graphql-request/src/createThunkFactory.ts
@@ -0,0 +1,11 @@
+import {Variables, type GraphQLClient} from "graphql-request";
+import {TypedDocumentNode} from "@graphql-typed-document-node/core";
+import {type VariablesAndRequestHeadersArgs} from "graphql-request/build/esm/types";
+
+export const createThunkFactory =
+  <TResult, TVars extends Variables = Variables>(
+    document: TypedDocumentNode<TResult, TVars>,
+  ) =>
+  (client: GraphQLClient) =>
+  (variables: VariablesAndRequestHeadersArgs<TVars>) =>
+    client.request(document, ...variables);
