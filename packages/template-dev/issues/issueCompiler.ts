0060f6eb95391acc31a50e096e77a950f27f6f67
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-13 16:31:22 +0530

    feat: :sparkles: add html document wrapper plugin (#35)

--- packages/template-dev/issues/issueCompiler.ts
+++ packages/template-dev/issues/issueCompiler.ts
@@ -1,8 +1,9 @@
 import {GetIssueQuery} from "@ipn/issues-gql";
 import {Issue} from "@ipn/templates";
 import {createCompileTask} from "@issue-notifier/template-compiler";
+import {wrapHtmlDocument} from "@issue-notifier/template-compiler-plugin-html";
 
 export const issueCompiler = (issue: GetIssueQuery) =>
   createCompileTask({
-    plugins: [],
+    plugins: [wrapHtmlDocument],
   })(Issue(issue));
