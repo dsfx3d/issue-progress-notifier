8c056b1b1d3a192fc8388603297c9061836720c5
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-14 11:01:22 +0530

    feat: change signature to reduce complexity

--- packages/purgecss/purgeCss.ts
+++ packages/purgecss/purgeCss.ts
@@ -1,26 +1,19 @@
 import {PurgeCSS} from "purgecss";
-import {ReaderTaskEither} from "fp-ts/lib/ReaderTaskEither";
 import {TPurgeRequest} from "./TPurgeRequest";
-import {flow} from "fp-ts/lib/function";
-import {map, tryCatchK} from "fp-ts/lib/TaskEither";
 
-export const purgeCss: ReaderTaskEither<TPurgeRequest, Error, string> = flow(
-  tryCatchK(
-    ({html, css}) =>
-      new PurgeCSS().purge({
-        content: [
-          {
-            raw: html,
-            extension: "html",
-          },
-        ],
-        css: [
-          {
-            raw: css,
-          },
-        ],
-      }),
-    e => e as Error,
-  ),
-  map(([{css}]) => css),
-);
+export async function purgeCss(request: TPurgeRequest): Promise<string> {
+  const [{css}] = await new PurgeCSS().purge({
+    content: [
+      {
+        raw: request.html,
+        extension: "html",
+      },
+    ],
+    css: [
+      {
+        raw: request.css,
+      },
+    ],
+  });
+  return css;
+}
