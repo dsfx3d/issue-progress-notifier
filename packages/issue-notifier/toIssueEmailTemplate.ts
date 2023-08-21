29a714fcc1fc76e1934a97090df755f16a3d8723
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-21 11:01:14 +0530

    feat: compile email template for `GetIssueQuery` (#45)

---
+++ packages/issue-notifier/toIssueEmailTemplate.ts
@@ -0,0 +1,34 @@
+import {Context} from "@actions/github/lib/context";
+import {GetIssueDocument} from "@ipn/issues-gql";
+import {Issue} from "@ipn/templates";
+import {TEmailTemplate} from "./TEmailTemplate";
+import {TaskEither, flatMap, map, tryCatch} from "fp-ts/lib/TaskEither";
+import {htmlCompiler} from "./htmlCompiler";
+import {pipe} from "fp-ts/lib/function";
+import {toGraphQLClient} from "./toGraphQLClient";
+
+export const toIssueEmailTemplate: (
+  context: Context,
+  accessToken: string,
+) => TaskEither<Error, TEmailTemplate> = (context, accessToken) =>
+  pipe(
+    tryCatch(
+      () =>
+        toGraphQLClient(accessToken).request(GetIssueDocument, {
+          issue: context.issue.number,
+          owner: context.repo.owner,
+          name: context.issue.repo,
+        }),
+      reason => new Error(String(reason)),
+    ),
+    flatMap(({repository}) =>
+      pipe(
+        Issue({repository}),
+        htmlCompiler,
+        map(html => ({
+          subject: `${repository?.issue?.titleHTML}`,
+          html,
+        })),
+      ),
+    ),
+  );
