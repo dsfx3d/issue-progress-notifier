8fc6959aeb776a21b5f67b0bf42cca57aee1fb69
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-21 11:11:46 +0530

    feat: remove env from program deps (#45)

--- packages/issue-notifier/TProgramDeps.ts
+++ packages/issue-notifier/TProgramDeps.ts
@@ -1,7 +1,6 @@
 import {TEmailTemplate} from "./TEmailTemplate";
 import {TaskEither} from "fp-ts/lib/TaskEither";
 import {Transporter} from "nodemailer";
-import {type env} from "./env";
 
 export type TProgramDeps = {
   readonly senderEmail: string;
