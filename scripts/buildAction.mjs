1323dc0d8517b5967e0f3a6eb323ed1475ffaf5e
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-21 21:08:37 +0530

    feat: :hammer: add script to build action

---
+++ scripts/buildAction.mjs
@@ -0,0 +1,12 @@
+import {buildSync} from "esbuild";
+
+buildSync({
+  entryPoints: ["issue-notifier/bin.ts"],
+  bundle: true,
+  outfile: "action.js",
+  platform: "node",
+  target: "node16",
+  minify: true,
+  sourcemap: false,
+  jsx: "preserve",
+});
