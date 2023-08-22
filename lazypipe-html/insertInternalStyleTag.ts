d71e404a211ace482aa4a4bd2cb80be704a5d891
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-22 12:31:36 +0530

    feat(insertInternalStyleTag): arg is a task of css string

--- lazypipe-html/insertInternalStyleTag.ts
+++ lazypipe-html/insertInternalStyleTag.ts
@@ -2,13 +2,15 @@ import {JSDOM} from "jsdom";
 import {type TStage} from "@issue-notifier/lazypipe";
 import {getOrCreateHead} from "./getOrCreateHead";
 
-export function insertInternalStyleTag(css: string): TStage<string> {
+export function insertInternalStyleTag(
+  getCss: () => Promise<string>,
+): TStage<string> {
   return async html => {
     const dom = new JSDOM(html);
     const document = dom.window.document;
     const head = getOrCreateHead(document);
     const style = document.createElement("style");
-    style.textContent = css;
+    style.textContent = await getCss();
     head.append(style);
     return dom.serialize();
   };
