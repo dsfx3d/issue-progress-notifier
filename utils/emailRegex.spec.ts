da549f7a7a62b09d9ceccaaa0305aa347f18846e
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-23 12:44:28 +0530

    refactor(emailRegex/spec): reduce duplications

--- utils/emailRegex.spec.ts
+++ utils/emailRegex.spec.ts
@@ -1,33 +1,36 @@
 import {emailRegex} from "./emailRegex";
 
+function matchExpect(
+  regex: RegExp,
+  inputs: string[],
+): jest.JestMatchers<RegExpMatchArray | null>[] {
+  return inputs.map(input => input.match(regex)).map(result => expect(result));
+}
+
 describe("emailRegex validation", () => {
   test("recognizes valid email addresses", () => {
-    const validEmails = [
+    for (const result of matchExpect(emailRegex, [
       "test@example.com",
       "test.user@example.com",
       "test-user@example.com",
       "test_user@example.com",
       "test@example.co.uk",
       "test@example.io",
-    ];
-
-    for (const email of validEmails) {
-      expect(email.match(emailRegex)).toBeTruthy();
+    ])) {
+      result.toBeTruthy();
     }
   });
 
   test("recognizes invalid email addresses", () => {
-    const invalidEmails = [
+    for (const result of matchExpect(emailRegex, [
       "test@",
       "test@example",
       "test@.com",
       "test@example..com",
       "test@.example.com",
       "test@example.com.",
-    ];
-
-    for (const email of invalidEmails) {
-      expect(email.match(emailRegex)).toBeFalsy();
+    ])) {
+      result.toBeFalsy();
     }
   });
 });
