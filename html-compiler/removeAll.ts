b777f4f67bf5410d9f29ead5dad3421f8c5c90ee
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-23 19:51:48 +0530

    :fire: remove unused files

--- html-compiler/removeAll.ts
+++
@@ -1,8 +0,0 @@
-import {JSDOM} from "jsdom";
-
-export const removeAll = (selector: string) => (html: string) => {
-  const dom = new JSDOM(html);
-  // eslint-disable-next-line unicorn/no-array-for-each
-  dom.window.document.querySelectorAll(selector)?.forEach(tag => tag.remove());
-  return dom.serialize();
-};
--- html-compiler/toInternalCss.ts
+++
@@ -1,8 +0,0 @@
-import {JSDOM} from "jsdom";
-
-export function toInternalCss(html: string): string {
-  const document = new JSDOM(html).window.document;
-  return [...document.querySelectorAll("style")]
-    .map(tag => tag.innerHTML)
-    .join(";");
-}
