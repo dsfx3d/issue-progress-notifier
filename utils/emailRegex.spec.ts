fc9e50fb98b694fa2227ba79d8d04f037c82abea
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-23 13:19:05 +0530

    refactor(emailRegex/spec): reduce duplications

--- utils/emailRegex.spec.ts
+++ utils/emailRegex.spec.ts
@@ -1,36 +1,56 @@
+import {Reader} from "fp-ts/lib/Reader";
 import {emailRegex} from "./emailRegex";
 
-function matchExpect(
-  regex: RegExp,
-  inputs: string[],
-): jest.JestMatchers<RegExpMatchArray | null>[] {
-  return inputs.map(input => input.match(regex)).map(result => expect(result));
+type JestRegexMatcher = jest.JestMatchers<RegExpMatchArray | null>;
+
+function toMatchers(regex: RegExp, inputs: string[]): JestRegexMatcher[] {
+  return inputs.map(input => expect(input.match(regex)));
 }
 
-describe("emailRegex validation", () => {
-  test("recognizes valid email addresses", () => {
-    for (const result of matchExpect(emailRegex, [
+const createBulkMatcher =
+  (regex: RegExp) =>
+  (
+    name: string,
+    expectation: Reader<JestRegexMatcher, void>,
+    inputs: string[],
+  ) => {
+    test(name, () => {
+      for (const matcher of toMatchers(regex, inputs)) {
+        expectation(matcher);
+      }
+    });
+  };
+
+const tests: [string, Reader<JestRegexMatcher, void>, string[]][] = [
+  [
+    "recognizes valid email addresses",
+    result => result.toBeTruthy(),
+    [
       "test@example.com",
       "test.user@example.com",
       "test-user@example.com",
       "test_user@example.com",
       "test@example.co.uk",
       "test@example.io",
-    ])) {
-      result.toBeTruthy();
-    }
-  });
-
-  test("recognizes invalid email addresses", () => {
-    for (const result of matchExpect(emailRegex, [
+    ],
+  ],
+  [
+    "recognizes invalid email addresses",
+    result => result.toBeFalsy(),
+    [
       "test@",
       "test@example",
       "test@.com",
       "test@example..com",
       "test@.example.com",
       "test@example.com.",
-    ])) {
-      result.toBeFalsy();
-    }
-  });
+    ],
+  ],
+];
+
+describe("emailRegex validation", () => {
+  const emailMatcher = createBulkMatcher(emailRegex);
+  for (const [name, expectation, inputs] of tests) {
+    emailMatcher(name, expectation, inputs);
+  }
 });
