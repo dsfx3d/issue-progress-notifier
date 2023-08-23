cf27612b509728f11c06f6e43ba9cec1a4c47482
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-23 14:14:46 +0530

    refactor(lazypipe): remove redundant function `applyStage`

--- lazypipe/applyStages.ts
+++ lazypipe/applyStages.ts
@@ -4,10 +4,10 @@ import {flatMap, of, tryCatchK} from "fp-ts/lib/TaskEither";
 import {pipe} from "fp-ts/lib/function";
 
 export function applyStages<TInput>(
-  plugins: TStage<TInput>[],
+  stage: TStage<TInput>[],
 ): ReaderTaskEither<TInput, Error, TInput> {
   return input =>
-    plugins
+    stage
       .map(stage => flatMap(tryCatchK(stage, error => error as Error)))
       // eslint-disable-next-line unicorn/no-array-reduce
       .reduce((acc, stage) => pipe(acc, stage), of<Error, TInput>(input));
