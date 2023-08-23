2c2e4983caeb6793a9a546b1fa9e4d0d3217bb10
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-23 15:10:09 +0530

    refactor(lazypipe): remove redundant function `applyStages`

--- lazypipe/createLazyPipe.ts
+++ lazypipe/createLazyPipe.ts
@@ -1,10 +1,14 @@
 import {ReaderTaskEither} from "fp-ts/lib/ReaderTaskEither";
 import {TStage} from "./TStage";
-import {applyStages} from "./applyStages";
-import {flow} from "fp-ts/lib/function";
+import {flatMap, of, tryCatchK} from "fp-ts/lib/TaskEither";
+import {identity, pipe} from "fp-ts/function";
 
 export function createLazyPipe<TInput>(
   ...stages: TStage<TInput>[]
-): ReaderTaskEither<TInput, Error, TInput> {
-  return flow(applyStages(stages));
+): ReaderTaskEither<TInput, unknown, TInput> {
+  return input =>
+    stages
+      .map(stage => flatMap(tryCatchK(stage, identity)))
+      // eslint-disable-next-line unicorn/no-array-reduce
+      .reduce((acc, stage) => pipe(acc, stage), of<unknown, TInput>(input));
 }
