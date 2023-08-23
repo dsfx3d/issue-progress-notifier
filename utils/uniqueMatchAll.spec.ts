45f8ab3c7cbbb8bf7ed1bfd2fd87862800405441
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-23 13:24:45 +0530

    refactor(uniqueMatchAll/spec): reduce duplications

--- utils/uniqueMatchAll.spec.ts
+++ utils/uniqueMatchAll.spec.ts
@@ -1,24 +1,35 @@
 import {uniqueMatchAll} from "./uniqueMatchAll";
 
+const tests: [string, RegExp, string, string[]][] = [
+  [
+    "returns unique matches from a string",
+    /(\w+@\w+\.\w+)/g,
+    "email1@example.com email2@example.com email1@example.com",
+    ["email1@example.com", "email2@example.com"],
+  ],
+  [
+    "returns an empty array when no matches are found",
+    /(\w+@\w+\.\w+)/g,
+    "no emails here",
+    [],
+  ],
+  [
+    "handles special characters correctly",
+    /(#\w+)/g,
+    "#tag1 #tag2 #tag1",
+    ["#tag1", "#tag2"],
+  ],
+];
+
+function runner(name: string, regex: RegExp, text: string, expected: string[]) {
+  test(name, () => {
+    const result = uniqueMatchAll(regex, text);
+    expect(result).toEqual(expected);
+  });
+}
+
 describe("uniqueMatchAll", () => {
-  it("returns unique matches from a string", () => {
-    const regex = /(\w+@\w+\.\w+)/g;
-    const text = "email1@example.com email2@example.com email1@example.com";
-    const result = uniqueMatchAll(regex, text);
-    expect(result).toEqual(["email1@example.com", "email2@example.com"]);
-  });
-
-  it("returns an empty array when no matches are found", () => {
-    const regex = /(\w+@\w+\.\w+)/g;
-    const text = "no emails here";
-    const result = uniqueMatchAll(regex, text);
-    expect(result).toEqual([]);
-  });
-
-  it("handles special characters correctly", () => {
-    const regex = /(#\w+)/g;
-    const text = "#tag1 #tag2 #tag1";
-    const result = uniqueMatchAll(regex, text);
-    expect(result).toEqual(["#tag1", "#tag2"]);
-  });
+  for (const [name, regex, text, expected] of tests) {
+    runner(name, regex, text, expected);
+  }
 });
