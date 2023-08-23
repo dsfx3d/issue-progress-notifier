4df6261598fd79a8b499904fca6fe12958cb2448
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-23 20:24:58 +0530

    refactor(compiler): reduce redundancy in minify stage

--- html-compiler/minify.ts
+++
@@ -1,6 +0,0 @@
-import {type Options, minify as minifyHtml} from "html-minifier-terser";
-import {type TStage} from "@issue-notifier/lazypipe";
-
-export function minify(options: Options): TStage<string> {
-  return async html => minifyHtml(html, options);
-}
