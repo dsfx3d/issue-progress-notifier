1023af225c68b793d81b51bc52c53a4591bb7bf3
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-24 21:30:16 +0530

    💩

--- bin.ts
+++ bin.ts
@@ -1,79 +1,89 @@
-import {GetIssueDocument} from "./graphql/lib/graphql";
-import {GraphQLClient} from "graphql-request";
-import {Issue} from "./templates/Issue";
-import {context} from "@actions/github";
-import {createTransport} from "nodemailer";
-import {emailRegex} from "./utils/emailRegex";
-import {env} from "./env";
-import {flatMap, map, matchE, tryCatch, tryCatchK} from "fp-ts/lib/TaskEither";
-import {htmlCompiler} from "./html-compiler/htmlCompiler";
-import {identity, pipe} from "fp-ts/lib/function";
-import {of} from "fp-ts/lib/Task";
-import {uniqueMatchAll} from "./utils/uniqueMatchAll";
+console.log("jhhjahja");
 
-type TSendEmailResult =
-  | {
-      success: false;
-      reason: unknown;
-    }
-  | {
-      success: true;
-    };
+// import {GetIssueDocument, GetIssueQuery} from "./graphql/lib/graphql";
+// import {GraphQLClient} from "graphql-request";
+// import {Issue} from "./templates/Issue";
+// import {Reader} from "fp-ts/lib/Reader";
+// import {TaskEither} from "fp-ts/Task";
+// import {context} from "@actions/github";
+// import {createTransport} from "nodemailer";
+// import {emailRegex} from "./utils/emailRegex";
+// import {env} from "./env";
+// import {flatMap, map, matchE, tryCatch, tryCatchK} from "fp-ts/lib/TaskEither";
+// import {htmlCompiler} from "./html-compiler/htmlCompiler";
+// import {identity, pipe} from "fp-ts/lib/function";
+// import {of} from "fp-ts/lib/Task";
+// import {uniqueMatchAll} from "./utils/uniqueMatchAll";
+// import Mail from "nodemailer/lib/mailer";
 
-const transporter = createTransport({
-  host: env.SMTP_HOST,
-  port: env.SMTP_PORT,
-  auth: {
-    user: env.SMTP_USER,
-    pass: env.SMTP_PASS,
-  },
-});
+// type TSendEmailResult =
+//   | {
+//       success: false;
+//       reason: unknown;
+//     }
+//   | {
+//       success: true;
+//     };
 
-const client = new GraphQLClient(context.graphqlUrl, {
-  headers: {
-    Authorization: `Bearer ${env.GITHUB_TOKEN}`,
-  },
-});
+// const transporter = createTransport({
+//   host: env.SMTP_HOST,
+//   port: env.SMTP_PORT,
+//   auth: {
+//     user: env.SMTP_USER,
+//     pass: env.SMTP_PASS,
+//   },
+// });
 
-const program = pipe(
-  tryCatch(
-    () =>
-      client.request(GetIssueDocument, {
-        issue: context.issue.number,
-        owner: context.repo.owner,
-        name: context.issue.repo,
-      }),
-    identity,
-  ),
-  flatMap(data =>
-    pipe(
-      Issue(data),
-      htmlCompiler,
-      map(html => ({
-        from: env.SMTP_FROM,
-        to: uniqueMatchAll(emailRegex, `${context.payload.issue?.body}`),
-        subject: `${data.repository?.issue?.author}`,
-        html,
-      })),
-    ),
-  ),
-  flatMap(tryCatchK(options => transporter.sendMail(options), identity)),
-  matchE<unknown, unknown, TSendEmailResult>(
-    reason => of({success: false, reason}),
-    () => of({success: true}),
-  ),
-);
+// const client = new GraphQLClient(context.graphqlUrl, {
+//   headers: {
+//     Authorization: `Bearer ${env.GITHUB_TOKEN}`,
+//   },
+// });
 
-// eslint-disable-next-line unicorn/prefer-top-level-await
-program()
-  .then(result => {
-    if (!result.success) {
-      console.error(result.reason);
-      throw new Error(String(result.reason));
-    }
-  })
-  // eslint-disable-next-line unicorn/prefer-top-level-await
-  .catch(error => {
-    console.error(error);
-    throw error;
-  });
+// const fetchData = (): Promise<GetIssueQuery> =>
+//   client.request(GetIssueDocument, {
+//     issue: context.issue.number,
+//     owner: context.repo.owner,
+//     name: context.issue.repo,
+//   });
+
+// const sendMail = async (options: Mail.Options) => transporter.sendMail(options);
+
+// const program = <TData>(
+//   fetchData: TaskEither<TData>,
+//   template: Reader<TData, string>,
+// ) =>
+//   pipe(
+//     tryCatch(fetchData, identity),
+//     flatMap(data =>
+//       pipe(
+//         template(data),
+//         htmlCompiler,
+//         map(html => ({
+//           from: env.SMTP_FROM,
+//           to: uniqueMatchAll(emailRegex, `${context.payload.issue?.body}`),
+//           subject: `${data.repository?.issue?.author}`,
+//           html,
+//         })),
+//       ),
+//     ),
+//     flatMap(tryCatchK(sendMail, identity)),
+//     matchE<unknown, unknown, TSendEmailResult>(
+//       reason => of({success: false, reason}),
+//       () => of({success: true}),
+//     ),
+//   );
+
+// // eslint-disable-next-line unicorn/prefer-top-level-await
+// program()
+//   .then(result => {
+//     if (!result.success) {
+//       console.error(result.reason);
+//       throw new Error(String(result.reason));
+//     }
+//   })
+//   // eslint-disable-next-line unicorn/prefer-top-level-await
+//   .catch(error => {
+//     console.error(error);
+//     throw error;
+//   });
