be2e57a3237b1a91073cc69be983756fb6584b8b
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-24 21:33:53 +0530

    💩

--- bin.ts
+++ bin.ts
@@ -1,51 +1,56 @@
-console.log("jhhjahja");
+import {GetIssueDocument, GetIssueQuery} from "./graphql/lib/graphql";
+import {GraphQLClient} from "graphql-request";
+import {Issue} from "./templates/Issue";
+import {Reader} from "fp-ts/lib/Reader";
+import {TaskEither} from "fp-ts/Task";
+import {context} from "@actions/github";
+import {createTransport} from "nodemailer";
+import {emailRegex} from "./utils/emailRegex";
+import {env} from "./env";
+import {flatMap, map, matchE, tryCatch, tryCatchK} from "fp-ts/lib/TaskEither";
+import {htmlCompiler} from "./html-compiler/htmlCompiler";
+import {identity, pipe} from "fp-ts/lib/function";
+import {of} from "fp-ts/lib/Task";
+import {uniqueMatchAll} from "./utils/uniqueMatchAll";
+import Mail from "nodemailer/lib/mailer";
 
-// import {GetIssueDocument, GetIssueQuery} from "./graphql/lib/graphql";
-// import {GraphQLClient} from "graphql-request";
-// import {Issue} from "./templates/Issue";
-// import {Reader} from "fp-ts/lib/Reader";
-// import {TaskEither} from "fp-ts/Task";
-// import {context} from "@actions/github";
-// import {createTransport} from "nodemailer";
-// import {emailRegex} from "./utils/emailRegex";
-// import {env} from "./env";
-// import {flatMap, map, matchE, tryCatch, tryCatchK} from "fp-ts/lib/TaskEither";
-// import {htmlCompiler} from "./html-compiler/htmlCompiler";
-// import {identity, pipe} from "fp-ts/lib/function";
-// import {of} from "fp-ts/lib/Task";
-// import {uniqueMatchAll} from "./utils/uniqueMatchAll";
-// import Mail from "nodemailer/lib/mailer";
+type TSendEmailResult =
+  | {
+      success: false;
+      reason: unknown;
+    }
+  | {
+      success: true;
+    };
 
-// type TSendEmailResult =
-//   | {
-//       success: false;
-//       reason: unknown;
-//     }
-//   | {
-//       success: true;
-//     };
+const transporter = createTransport({
+  host: env.SMTP_HOST,
+  port: env.SMTP_PORT,
+  auth: {
+    user: env.SMTP_USER,
+    pass: env.SMTP_PASS,
+  },
+});
 
-// const transporter = createTransport({
-//   host: env.SMTP_HOST,
-//   port: env.SMTP_PORT,
-//   auth: {
-//     user: env.SMTP_USER,
-//     pass: env.SMTP_PASS,
-//   },
-// });
+console.log("transporter", transporter);
 
-// const client = new GraphQLClient(context.graphqlUrl, {
-//   headers: {
-//     Authorization: `Bearer ${env.GITHUB_TOKEN}`,
-//   },
-// });
+const client = new GraphQLClient(context.graphqlUrl, {
+  headers: {
+    Authorization: `Bearer ${env.GITHUB_TOKEN}`,
+  },
+});
 
-// const fetchData = (): Promise<GetIssueQuery> =>
-//   client.request(GetIssueDocument, {
-//     issue: context.issue.number,
-//     owner: context.repo.owner,
-//     name: context.issue.repo,
-//   });
+console.log("client", client);
+
+const fetchData = (): Promise<GetIssueQuery> =>
+  client.request(GetIssueDocument, {
+    issue: context.issue.number,
+    owner: context.repo.owner,
+    name: context.issue.repo,
+  });
+
+// eslint-disable-next-line unicorn/prefer-top-level-await
+fetchData().then(console.log).catch(console.error);
 
 // const sendMail = async (options: Mail.Options) => transporter.sendMail(options);
 
