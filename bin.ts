de37313a28b9bcc79fe9fb748436050f1c8f94b1
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-25 20:46:48 +0530

    refactor(bin): reduce cognitive complexity

--- bin.ts
+++ bin.ts
@@ -1,91 +1,46 @@
-import {GetIssueDocument, GetIssueQuery} from "./graphql/lib/graphql";
 import {GraphQLClient} from "graphql-request";
-import {Issue} from "./templates/Issue";
 import {context} from "@actions/github";
 import {createTransport} from "nodemailer";
 import {emailRegex} from "./utils/emailRegex";
-import {flatMap, map, matchE, tryCatch, tryCatchK} from "fp-ts/lib/TaskEither";
-import {htmlCompiler} from "./html-compiler/htmlCompiler";
-import {identity, pipe} from "fp-ts/lib/function";
-import {of} from "fp-ts/lib/Task";
+import {sendMail} from "./action/sendMail";
+import {toAction} from "./action/toAction";
+import {toGetIssueThunk} from "./issue/toGetIssueThunk";
+import {toIssueTemplate} from "./issue/toIssueTemplate";
 import {uniqueMatchAll} from "./utils/uniqueMatchAll";
-import Mail from "nodemailer/lib/mailer";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
-
-const env = process.env;
-console.log("SMTP_HOST", env.SMTP_HOST);
-
-type TSendEmailResult =
-  | {
-      success: false;
-      reason: unknown;
-    }
-  | {
-      success: true;
-    };
-
-const transporter = createTransport({
-  host: env.SMTP_HOST,
-  port: env.SMTP_PORT,
-  auth: {
-    user: env.SMTP_USER,
-    pass: env.SMTP_PASS,
-  },
-} as SMTPTransport.Options);
 
 const client = new GraphQLClient(context.graphqlUrl, {
   headers: {
-    Authorization: `Bearer ${env.ACCESS_TOKEN}`,
+    Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
   },
 });
 
-const fetchData = (): Promise<GetIssueQuery> =>
-  client.request(GetIssueDocument, {
-    issue: context.issue.number,
+const transporter = createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+} as SMTPTransport.Options);
+
+const action = toAction(
+  toGetIssueThunk(client, {
     owner: context.repo.owner,
-    name: context.issue.repo,
-  });
-
-const sendMail = async (options: Mail.Options) => transporter.sendMail(options);
-
-const program = pipe(
-  tryCatch(fetchData, identity),
-  flatMap(data =>
-    pipe(
-      Issue(data),
-      htmlCompiler,
-      map(html => ({
-        from: env.SMTP_FROM,
-        to: uniqueMatchAll(emailRegex, `${context.payload.issue?.body}`),
-        subject: `${data.repository?.issue?.title}`,
-        html,
-      })),
-    ),
-  ),
-  flatMap(tryCatchK(sendMail, identity)),
-  matchE<unknown, unknown, TSendEmailResult>(
-    reason => of({success: false, reason}),
-    () => of({success: true}),
-  ),
+    name: context.repo.repo,
+    issue: context.issue.number,
+  }),
+  data => ({
+    from: process.env.SMTP_USER,
+    to: uniqueMatchAll(emailRegex, `${context.payload.issue?.body}`),
+    subject: `${data.repository?.issue?.title}`,
+    html: toIssueTemplate(data),
+  }),
+  sendMail(transporter),
 );
-
 // eslint-disable-next-line unicorn/prefer-top-level-await
-program()
-  .then(result => {
-    console.log(
-      "recipients",
-      `${context.payload.issue?.body}`,
-      JSON.stringify(
-        uniqueMatchAll(emailRegex, `${context.payload.issue?.body}`),
-      ),
-    );
-    if (!result.success) {
-      console.error(result.reason);
-      throw new Error(String(result.reason));
-    }
-  })
-  // eslint-disable-next-line unicorn/prefer-top-level-await
-  .catch(error => {
-    console.error(error);
-    throw error;
-  });
+action().then(result => {
+  if (!result.success) {
+    console.error(result.reason);
+  }
+});
