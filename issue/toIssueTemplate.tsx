1a278978ed1696cb9993fcbd19da31b6cb95d729
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-25 18:35:53 +0530

    feat(issue): add issue template

---
+++ issue/toIssueTemplate.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as elements from "typed-html";
+import {GetIssueQuery} from "../lib/graphql";
+
+export function toIssueTemplate({repository}: GetIssueQuery): string {
+  return (
+    <div>
+      <h1>Issue</h1>
+      <p>{repository?.issue?.author?.login}</p>
+      <div>{repository?.issue?.bodyHTML}</div>
+    </div>
+  );
+}
