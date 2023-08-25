ad2371833705f52a77c2ce13c0181518bf41ee2f
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-25 20:44:51 +0530

    feat: add action factory

--- action/toAction.ts
+++ action/toAction.ts
@@ -1,16 +1,25 @@
 import {LazyArg, identity, pipe} from "fp-ts/lib/function";
 import {Reader} from "fp-ts/lib/Reader";
 import {TActionResult} from "./TActionResult";
-import {TaskEither, flatMap, matchE, tryCatchK} from "fp-ts/lib/TaskEither";
+import {flatMap, map, matchE, tryCatch, tryCatchK} from "fp-ts/lib/TaskEither";
+import {htmlCompiler} from "../html-compiler/htmlCompiler";
 import {of} from "fp-ts/lib/Task";
 import Mail from "nodemailer/lib/mailer";
 
-export function toAction(
-  options: TaskEither<unknown, Mail.Options>,
+export function toAction<T>(
+  fetchData: LazyArg<Promise<T>>,
+  toMailOptions: Reader<T, Mail.Options>,
   sendMail: Reader<Mail.Options, Promise<unknown>>,
 ): LazyArg<Promise<TActionResult>> {
   return pipe(
-    options,
+    tryCatch(fetchData, identity),
+    map(toMailOptions),
+    flatMap(options =>
+      pipe(
+        htmlCompiler(options.html as string),
+        map(html => ({...options, html})),
+      ),
+    ),
     flatMap(tryCatchK(sendMail, identity)),
     matchE<unknown, unknown, TActionResult>(
       reason => of({success: false, reason}),
