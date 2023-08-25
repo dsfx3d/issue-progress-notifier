e1a1770f99d982b4a1ceddfead439b0ce08295a8
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-25 23:32:30 +0530

    feat: remove html compilation step

--- action/toAction.ts
+++ action/toAction.ts
@@ -1,19 +1,11 @@
 import {Context} from "@actions/github/lib/context";
 import {GraphQLClient} from "graphql-request";
-import {IO, map as mapIO} from "fp-ts/lib/IO";
+import {IO, map} from "fp-ts/lib/IO";
 import {Reader} from "fp-ts/lib/Reader";
 import {TActionResult} from "./TActionResult";
 import {TaskEither as Task, of} from "fp-ts/lib/Task";
-import {
-  flatMap,
-  fromIO,
-  map,
-  matchE,
-  tryCatch,
-  tryCatchK,
-} from "fp-ts/lib/TaskEither";
+import {flatMap, fromIO, matchE, tryCatchK} from "fp-ts/lib/TaskEither";
 import {identity, pipe} from "fp-ts/lib/function";
-import {toHtml} from "../html-compiler/toHtml";
 import Mail from "nodemailer/lib/mailer";
 
 // eslint-disable-next-line max-params
@@ -21,21 +13,15 @@ export function toAction<T>(
   contextIo: IO<Context>,
   toClient: Reader<Context, GraphQLClient>,
   fetchData: Reader<GraphQLClient, Promise<T>>,
-  toMailOptions: Reader<T, Mail.Options>,
+  toMailOptions: Reader<T, Promise<Mail.Options>>,
   sendMail: Reader<Mail.Options, Promise<unknown>>,
 ): Task<TActionResult> {
   return pipe(
     contextIo,
-    mapIO(toClient),
+    map(toClient),
     fromIO,
     flatMap(tryCatchK(fetchData, identity)),
-    map(toMailOptions),
-    flatMap(options =>
-      pipe(
-        tryCatch(() => toHtml(options.html as string), identity),
-        map(html => ({...options, html})),
-      ),
-    ),
+    flatMap(tryCatchK(toMailOptions, identity)),
     flatMap(tryCatchK(sendMail, identity)),
     matchE<unknown, unknown, TActionResult>(
       reason => of({success: false, reason}),
