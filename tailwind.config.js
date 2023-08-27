140d3a1e8f1f4ae981133998f98d15378b07fb5c
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-27 13:56:46 +0530

    chore: add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable unicorn/prefer-module */
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
