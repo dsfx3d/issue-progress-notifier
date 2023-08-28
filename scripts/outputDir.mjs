3b13cf3b274a873abb9bb7b05acd81e9d7d1cf40
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-28 23:44:27 +0530

    feat(outputDir): resolve absolute path

--- scripts/outputDir.mjs
+++ scripts/outputDir.mjs
@@ -1,1 +1,7 @@
-export const outputDir = "lib";
+import {dirname, resolve} from "node:path";
+import {fileURLToPath} from "node:url";
+
+export const outputDir = resolve(
+  dirname(fileURLToPath(import.meta.url)),
+  "lib",
+);
