6259b4b18572d0a6692c2b8a69efe519f90f794b
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-28 15:24:18 +0530

    chore: set output path of generated github css

--- scripts/generateGitHubCss.mjs
+++ scripts/generateGitHubCss.mjs
@@ -1,8 +1,9 @@
+import {githubCss} from "./githubCss.mjs";
 import {writeFileSync} from "node:fs";
 import generateGitHubMarkdownCss from "generate-github-markdown-css";
 
 async function run() {
-  writeFileSync("lib/styles.css", await generateGitHubMarkdownCss());
+  writeFileSync(githubCss, await generateGitHubMarkdownCss());
 }
 
 run();
