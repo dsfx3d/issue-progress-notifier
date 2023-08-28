d56a3e9408208403bf2a65daca2221eecd1ed981
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-28 20:49:48 +0530

    feat: add template for issue head

--- issue/toIssueHead.tsx
+++ issue/toIssueHead.tsx
@@ -1,18 +1,27 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as elements from "typed-html";
+import {IssueHeadFragment} from "../lib/graphql";
 
-export function toIssueHead() {
+export function toIssueHead({
+  title,
+  number,
+  state,
+  author,
+  createdAt,
+}: IssueHeadFragment): string {
   return (
     <div class="flex flex-col gap-2 font-sans border-b-2 pb-4">
       <h1 class="text-3xl font-medium">
-        this is the title
-        <span class="text-gray-500 text-2xl font-light">#23</span>
+        {title}
+        <span class="text-gray-500 text-2xl font-light">#{number}</span>
       </h1>
       <div class="flex gap-2 content-center items-center">
-        <span class="badge badge-success py-4 font-semibold">Open</span>
+        <span class="badge badge-success py-4 font-semibold lowercase">
+          {state}
+        </span>
         <span class="text-sm">
-          <b class="font-medium">dsfx3d</b> opened this issue 2 days ago · 1
-          comment
+          <b class="font-medium">{author?.login}</b> opened this issue at{" "}
+          {createdAt}
         </span>
       </div>
     </div>
