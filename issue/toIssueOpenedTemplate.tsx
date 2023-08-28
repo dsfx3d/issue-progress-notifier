b128d843aa0553e1b8b03c99d920eb94ada608f9
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-28 23:37:56 +0530

    :label: :hanky:

--- issue/toIssueOpenedTemplate.tsx
+++ issue/toIssueOpenedTemplate.tsx
@@ -1,14 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as elements from "typed-html";
-import {IssueOpenedQuery} from "$lib/graphql";
+import {IssueHeadFragment, IssueOpenedQuery} from "$lib/graphql";
 import {toIssueHeadTemplate} from "./toIssueHeadTemplate";
 
 export function toIssueOpenedTemplate({repository}: IssueOpenedQuery): string {
   return (
     <div>
-      {toIssueHeadTemplate(
-        repository!.issue![" $fragmentRefs"]!.IssueHeadFragment!,
-      )}
+      {toIssueHeadTemplate(repository!.issue! as unknown as IssueHeadFragment)}
       <div>{repository?.issue?.bodyHTML}</div>
     </div>
   );
