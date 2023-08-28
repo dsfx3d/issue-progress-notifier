b5f38d6d9d1ece6bc0d5256cec8aebcd84286c88
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-28 15:12:46 +0530

    chore: add script to generate tailwind css

---
+++ scripts/generateTailwindCss.mjs
@@ -0,0 +1,22 @@
+import {tailwindCss} from "./tailwindCss.mjs";
+import {writeFileSync} from "node:fs";
+import postcss from "postcss";
+import tailwindcss from "tailwindcss";
+
+const generateTailwindCss = async () => {
+  const inputCss = `
+@tailwind components;
+@tailwind utilities;
+`;
+  const output = await postcss([tailwindcss("tailwind.config.js")]).process(
+    inputCss,
+    {
+      from: undefined,
+    },
+  );
+  return output.css;
+};
+
+generateTailwindCss().then(css => {
+  writeFileSync(tailwindCss, css);
+});
