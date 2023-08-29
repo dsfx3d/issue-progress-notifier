b4c698ef15866f1d19118745985bf589bdf2f058
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-30 00:18:33 +0530

    chore(tsconfig): add declaration for `generate-github-markdown-css`

--- types/global.d.ts
+++ types/global.d.ts
@@ -2,3 +2,7 @@ declare module "$lib/styles.css?inline" {
   const content: string;
   export default content;
 }
+
+declare module "generate-github-markdown-css" {
+  export function getCss(): string;
+}
