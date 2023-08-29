ac66c8b9741479fce1b4f918f86f5e0b186de0f4
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-29 10:11:57 +0530

    fix: incorrect sender email

--- bin.ts
+++ bin.ts
@@ -10,9 +10,6 @@ import {toIssueOpenedTemplate} from "./issue/toIssueOpenedTemplate";
 import {uniqueMatchAll} from "./utils/uniqueMatchAll";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
 
-console.log("from", process.env.SMTP_FROM);
-console.log("to", uniqueMatchAll(emailRegex, `${context.payload.issue?.body}`));
-
 const action = toAction(
   () =>
     new GraphQLClient(context.graphqlUrl, {
@@ -27,26 +24,24 @@ const action = toAction(
       issue: context.issue.number,
     }),
   async data => ({
-    from: process.env.SMTP_USER,
+    from: process.env.SMTP_FROM,
     to: uniqueMatchAll(emailRegex, `${context.payload.issue?.body}`),
-    subject: `${data.repository?.issue?.[" $fragmentRefs"]?.IssueHeadFragment.title}`,
+    subject: `${data.repository?.issue?.title}`,
     html: await toHtml({
       body: toIssueOpenedTemplate(data),
       css: `${await readFile("./lib/tailwind.css", "utf8")}
 ${await readFile("./lib/github.css", "utf8")}`,
     }),
   }),
-  options => {
-    console.log(options);
-    return createTransport({
+  options =>
+    createTransport({
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
       },
-    } as SMTPTransport.Options).sendMail(options);
-  },
+    } as SMTPTransport.Options).sendMail(options),
 );
 
 action().then(result => {
