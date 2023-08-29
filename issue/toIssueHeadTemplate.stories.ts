a27332caff880b224b5ee1301ba174171e459c6b
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-29 23:30:09 +0530

    chore(IssueHead): add story

---
+++ issue/toIssueHeadTemplate.stories.ts
@@ -0,0 +1,32 @@
+import {IssueHeadFragment, IssueState} from "../lib/graphql";
+import {toHtml} from "../html-compiler/toHtml";
+import {toIssueHeadTemplate} from "./toIssueHeadTemplate";
+import css from "../lib/styles.css?inline";
+import type {Meta, StoryObj} from "@storybook/html";
+
+// More on how to set up stories at: https://storybook.js.org/docs/html/writing-stories/introduction#default-export
+const meta = {
+  title: "Molecules/IssueHead",
+  tags: ["autodocs"],
+  render: args =>
+    toHtml({
+      body: toIssueHeadTemplate(args),
+      css,
+    }),
+} satisfies Meta<IssueHeadFragment>;
+
+export default meta;
+type Story = StoryObj<IssueHeadFragment>;
+
+// More on writing stories with args: https://storybook.js.org/docs/html/writing-stories/args
+export const Primary: Story = {
+  args: {
+    author: {
+      login: "octocat",
+    },
+    title: "Hello World",
+    createdAt: "2021-01-01T00:00:00Z",
+    number: 1,
+    state: IssueState.Open,
+  },
+};
--- issue/toIssueHeadTemplate.tsx
+++ issue/toIssueHeadTemplate.tsx
@@ -10,20 +10,31 @@ export function toIssueHeadTemplate({
   createdAt,
 }: IssueHeadFragment): string {
   return (
-    <div class="flex flex-col gap-2 font-sans border-b-2 pb-4">
-      <h1 class="text-3xl font-medium">
-        {title}
-        <span class="text-gray-500 text-2xl font-light">#{number}</span>
-      </h1>
-      <div class="flex gap-2 content-center items-center">
-        <span class="badge badge-success py-4 font-semibold lowercase">
-          {state}
-        </span>
-        <span class="text-sm">
-          <b class="font-medium">{author?.login}</b> opened this issue at{" "}
-          {new Date(createdAt)}
-        </span>
-      </div>
-    </div>
+    <table
+      width="100%"
+      cellpadding="0"
+      cellspacing="0"
+      class="border-b-2 pb-4 font-sans"
+    >
+      <tr>
+        <td colspan="2">
+          <div class="text-3xl font-medium pb-2">
+            {title}
+            <span class="text-gray-500 text-2xl font-light">#{number}</span>
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <div class="pb-4">
+            <span class="badge badge-success py-2 font-semibold lowercase">
+              {state}
+            </span>
+            <b class="font-medium">{author?.login}</b> opened this issue at{" "}
+            {new Date(createdAt)}
+          </div>
+        </td>
+      </tr>
+    </table>
   );
 }
