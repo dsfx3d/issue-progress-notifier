101a0f4c47759bb6d63f76c9a61f10fce97ca4f2
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-09-01 01:03:38 +0530

    feat: encode list of subscribers before output

--- action/setActionOutput.ts
+++ action/setActionOutput.ts
@@ -2,7 +2,10 @@ import {TActionOutput} from "./TActionOutput";
 import {setOutput} from "@actions/core";
 
 export function setActionOutput(output: TActionOutput) {
-  setOutput("subscribers", output.subscribers.join(","));
+  setOutput(
+    "subscribers",
+    Buffer.from(`${output.subscribers.join(",")}`).toString("base64"),
+  );
   setOutput("subject", output.subject);
   setOutput("bodyHtml", output.bodyHtml);
   setOutput("bodyText", output.bodyText);
