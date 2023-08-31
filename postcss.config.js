4b3b7c48524f2775c4ea901d9d33c57de1a2deb2
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-31 12:32:10 +0530

    chore(postcss): remove premature optimizations

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,5 @@
  * @type {import('postcss').ProcessOptions}
  */
 module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    // require("postcss-css-variables"),
-    require("@fullhuman/postcss-purgecss")({
-      content: ["./**/*.tsx"],
-    }),
-    // require("postcss-css-variables"),
-  ],
+  plugins: [require("tailwindcss")],
 };
