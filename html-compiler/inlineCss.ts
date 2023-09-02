735eb80b3995fb2b7397a264beda7be6ef956e2f
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-09-02 22:44:41 +0530

    feat(types): add type for html compiler option

--- html-compiler/inlineCss.ts
+++ html-compiler/inlineCss.ts
@@ -1,7 +1,7 @@
-import {TCompilerOptions} from "./TCompilerOptions";
+import {THtmlOptions} from "./THtmlOptions";
 import juice from "juice";
 
-export function inlineCss({body, css}: TCompilerOptions): string {
+export function inlineCss({body, css}: THtmlOptions): string {
   juice.excludedProperties = ["--tw-shadow"];
   return juice.inlineContent(body, css, {
     removeStyleTags: false,
