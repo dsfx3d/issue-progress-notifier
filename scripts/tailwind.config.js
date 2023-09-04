b747a14df6b4244fa6b2d8478164a328f079bee2
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-09-04 23:21:25 +0530

    feat(tailwind): add github themes

--- scripts/tailwind.config.js
+++ scripts/tailwind.config.js
@@ -5,7 +5,35 @@ module.exports = {
   content: ["**/*.tsx", "**/*/toHtml.ts"],
   plugins: [require("tailwindcss-email-variants"), require("daisyui")],
   daisyui: {
-    theme: ["emerald"],
+    themes: [
+      {
+        "github-light": {
+          primary: "#0366d6", // Blue
+          secondary: "#f6f8fa", // Light Gray
+          accent: "#6f42c1", // GitHub Purple
+          neutral: "#586069", // Dark Gray
+          info: "#24292e", // Almost Black
+          warning: "#ffd33d", // Yellow
+          error: "#d73a49", // Red
+          success: "#28a745", // Green
+          "base-100": "#ffffff", // White
+        },
+      },
+      {
+        "github-dark": {
+          primary: "#0d1117", // Almost Black
+          secondary: "#161b22", // Dark Gray
+          accent: "#6f42c1", // GitHub Purple
+          neutral: "#c9d1d9", // Light Gray
+          info: "#f0f6fc", // Almost White
+          warning: "#dbab09", // Dark Yellow
+          error: "#f85149", // Red
+          success: "#238636", // Green
+          "base-100": "#21262d", // Darker Gray
+        },
+      },
+    ],
+    darkTheme: "github-dark",
   },
   corePlugins: {
     preflight: false,
