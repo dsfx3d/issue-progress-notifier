9f773fdd79b8cda34e0b856d668f22073d54e6df
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-09-06 12:58:58 +0530

    feat: expose templates and html compiler

---
+++ index.ts
@@ -0,0 +1,7 @@
+export * from "./html-compiler/toHtml";
+export * from "./issue/Issue";
+export * from "./issue/IssueTitle";
+export * from "./issue/RepoAvatar";
+export * from "./issue/State";
+export * from "./issue/StateAuthor";
+export * from "./issue/StateBadge";
